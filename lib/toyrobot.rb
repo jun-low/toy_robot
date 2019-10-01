f46723303c7d55af14638b1bb6222233c573382b
jun low <zhijun.low@gmail.com>
2019-10-02 09:29:21 +1000

    updated required files

--- lib/toyrobot.rb
+++ lib/toyrobot.rb
@@ -1,3 +1,20 @@
-class Toyrobot
+require_relative 'robot'
+require_relative 'robot_runner'
 
-end
+# initializes a new a new robot
+Robot.new
+
+puts 'Welcome to toy robot simulator!'
+
+# # keeps reading commands from input until user types EXIT
+# loop do
+#   puts 'input command (EXIT to quit): '
+
+#   input = gets
+
+#   command = commands.parse(input)
+
+#   command&.execute unless command.nil?
+
+#   break if input =~ /exit/i
+# end
